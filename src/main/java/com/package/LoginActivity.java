9fd8f55d8796653c8539fc68c351ca937a3184fd
Paulo McNally <paulomcnally@gmail.com>
2015-07-24 01:50:05 -0600

    sources

---
+++ src/main/java/com/package/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.package;
+
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.WindowManager;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    private void init() {
+        // EditText
+
+        AppCompatEditText email = (AppCompatEditText) findViewById(R.id.email);
+        AppCompatEditText password = (AppCompatEditText) findViewById(R.id.password);
+
+        // EditText line color
+        email.getBackground().setColorFilter(getResources().getColor(R.color.md_light_blue_500), PorterDuff.Mode.SRC_ATOP);
+        password.getBackground().setColorFilter(getResources().getColor(R.color.md_light_blue_500), PorterDuff.Mode.SRC_ATOP);
+
+        // Button
+        ColorStateList csl = new ColorStateList(new int[][]{new int[0]}, new int[]{getResources().getColor(R.color.md_light_blue_500)});
+
+        AppCompatButton signIn = (AppCompatButton) findViewById(R.id.buttonSignIn);
+        AppCompatButton signUp = (AppCompatButton) findViewById(R.id.buttonSignUp);
+
+        signIn.setSupportBackgroundTintList(csl);
+        signUp.setSupportBackgroundTintList(csl);
+
+        // StatusBar color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.md_light_blue_700));
+        }
+    }
+
+}
